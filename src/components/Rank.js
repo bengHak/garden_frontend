c076bc10ae736e9ae4be1b0fb092ec1e14482720
bengHak <41236155+bengHak@users.noreply.github.com>
2019-12-22 02:23:55 +0900

    before backend

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -2,11 +2,56 @@ import React from 'react';
 
 class Rank extends React.Component{
     render() {
+        let rankList = [];
         return (
             <>
+            <First></First>
+            <Second></Second>
+            <Third></Third>
+            {
+                rankList.map(row => {
+                    return (
+                        <RankRow
+                        />
+                    );
+                })
+            }
             </>
         )
     }
 }
+
+
+const First = () => {
+    return (
+        <div
+            style={{width:'80%', padding: '20px'}}
+        >
+        </div>
+    );
+}
+
+const Second = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+const Third = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+const RankRow = () => {
+
+    return (
+        <>
+        </>
+    );
+};
+
 
 export default Rank;
--- src/components/TotalAttendance.js
+++ src/components/TotalAttendance.js
@@ -2,54 +2,11 @@ import React from 'react';
 
 class TotalAttendance extends React.Component{
     render() {
-        let rankList = [];
         return (
             <>
-            <First></First>
-            <Second></Second>
-            <Third></Third>
-            {
-                rankList.map(row => {
-                    return (
-                        <RankRow
-                        />
-                    );
-                })
-            }
             </>
         )
     }
 }
-
-const First = () => {
-    return (
-        <div
-            style={{width:'80%', padding: '20px'}}
-        >
-        </div>
-    );
-}
-
-const Second = () => {
-    return (
-        <>
-        </>
-    );
-};
-
-const Third = () => {
-    return (
-        <>
-        </>
-    );
-};
-
-const RankRow = () => {
-
-    return (
-        <>
-        </>
-    );
-};
 
 export default TotalAttendance;
