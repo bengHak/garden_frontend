305b5c3af00aea482836c567ef8a88430ec8b1d5
bengHak <41236155+bengHak@users.noreply.github.com>
2019-12-31 00:17:25 +0900

    달성률 그래프 기능 구현

--- src/components/AchiveGraph.js
+++ src/components/AchiveGraph.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import {connect} from "react-redux";
+import TodayAttendance from './TodayAttendance';
 
 class AchiveGraph extends React.Component {
     render(){
-        let total_attendance;
-        let toady_attendance;
+        let total_attendance_count=0;
+        let today_attendance_count=0;
+        const total_attendance = 21 * 24;
+        const today_attendance = 24; 
+
+        const attendance_data  = this.props.attendance;
+        // 최적화 어떻게 하면 좋을지???
+        // console.log(attendance_data);
+
+        const today = new Date();
+        const todayString = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate());
+
+        for(let i=0; i<attendance_data.length; ++i){
+            total_attendance_count += Object.keys(attendance_data[i]['attendance']).length;
+            if(todayString in attendance_data[i]['attendance']){
+                today_attendance_count += 1;
+            }
+        }
 
         return (
             <div
@@ -15,9 +32,9 @@ class AchiveGraph extends React.Component {
                     padding: '20px'
                 }}
             >
-                <CustomGraph title="나의" color="#99c0ff"/>
-                <CustomGraph title="오늘" color="#fc857e"/>
-                <CustomGraph title="전체" color="#84db87"/>
+                <CustomGraph title="나의" color="#99c0ff" percentage={18}/>
+                <CustomGraph title="오늘" color="#fc857e" percentage={Math.floor(today_attendance_count/today_attendance*100)}/>
+                <CustomGraph title="전체" color="#84db87" percentage={Math.floor(total_attendance_count/total_attendance*100)}/>
             </div>
         )
     }
@@ -25,7 +42,7 @@ class AchiveGraph extends React.Component {
 
 class CustomGraph extends React.Component{
     render(){
-        const percentage = 66;
+        const percentage = this.props.percentage;
         const thisColor = this.props.color;
         const thisWidth = window.innerWidth > 700 ? '20vw' : '70vw';
         return (
