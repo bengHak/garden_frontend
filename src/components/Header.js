51dbeccfc35e54880211a857413fb277f7b65b83
bengHak <41236155+bengHak@users.noreply.github.com>
2020-01-01 03:51:36 +0900

    기간 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ class Header extends React.Component{
     render() {
         return (
             <>
-                <div style={{fontSize: '4rem', margin: '50px'}}>정원사들_HUFS</div>
+                <div style={{fontSize: '4rem', margin: '50px 0 0 0'}}>정원사들_HUFS</div>
+                <h4>191223 ~ 200113</h4>
             </>
         )
     }
