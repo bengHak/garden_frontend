6c4c12f7c7c0eaf5daa805b7309b95dcdff352a7
bengHak <qkwl4678@naver.com>
2020-01-15 17:20:23 +0900

    2기 시작

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component{
         return (
             <>
                 <div style={{fontSize: '4rem', margin: '50px 0 0 0'}}>정원사들_HUFS</div>
-                <h4>191223 ~ 200113</h4>
+                <h4>200115 ~ 200205</h4>
             </>
         )
     }
