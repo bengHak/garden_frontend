d51d4d192642230e042907140ccb78a5f2dade36
bengHak <41236155+bengHak@users.noreply.github.com>
2020-01-17 19:24:01 +0900

    사용하지 않는 함수 제거

--- src/components/TotalAttendance.js
+++ src/components/TotalAttendance.js
@@ -4,48 +4,21 @@ import {connect} from "react-redux";
 
 class TotalAttendance extends React.Component{
     state = {
-        start_date: '2019-12-23',
+        start_date: '2019-01-15',
         period: 21
     };
-
-    componentDidMount() {
-        this.buildCalendar();
-    }
 
     buildCalendar = () => {
         const start = this.state.start_date;
         const attendance = this.props.attendance;
         const { dispatch } = this.props;
-
-        let date = new Date(this.state.start_date);
-        for(let i=0; i<this.state.period; ++i) {
-            let month = ''+(date.getMonth()+1);
-            let day = ''+date.getDate();
-
-            if(month.length < 2)
-                month = '0' + month;
-            if(day.length < 2)
-                day = '0' + day;
-            const dateString = date.getFullYear()+'-'+month+'-'+day;
-
-            // console.log(dateString);
-            dispatch({type: 'DATE_ADD_REQUEST', payload: {'date':dateString}});
-            date.setDate(date.getDate()+1);
-        }
+        console.log(start);
+        console.log(attendance);
+        console.log("-----------------------");
     };
-
-    sortByDate = (dates) => {
-        dates.sort((a, b)=>{
-            return new Date(a.date) - new Date(b.date);
-        });
-    };
-
 
     render() {
-        if(this.props.dates.length === 21){
-            this.sortByDate(this.props.dates);
-            console.log(this.props.dates);
-        }
+        this.buildCalendar();
         return (
             <div className="TotalAttendance">
                 <br/>
@@ -55,44 +28,9 @@ class TotalAttendance extends React.Component{
     }
 }
 
-class DailyAttendance extends React.Component {
-    render() {
-        const progress = 66;
-        const top = (100-progress) + '%';
-        return (
-            <div className='daily-attendance'>
-                <div
-                    className='daily-progress'
-                    style={{
-                        position: 'absolute',
-                        backgroundColor:'#84db87',
-                        borderRadius: '5px',
-                        top: top,
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        fontSize: '1.1rem',
-                    }}
-                >
-                    {progress}%
-                </div>
-                <div
-                    style={{
-                        position: 'absolute',
-                        bottom: '-20px',
-                        left: 0,
-                        right: 0,
-                    }}
-                >12/31</div>
-            </div>
-        );
-    }
-}
-
 const mapStateToProps = state => ({
     users: state.users,
     attendance: state.attendance,
-    dates: state.dates,
 });
 
 export default connect(
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,10 @@
 import users from './user';
 import attend from './attend';
-import date from './dates';
-
 import { combineReducers } from 'redux'
 
 const rootReducers = combineReducers({
     users: users,
     attendance: attend,
-    dates: date,
 });
 
 export default rootReducers;
